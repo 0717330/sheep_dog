5f88aef17aa93d082cfb6e4045aa2bc4a4e40344
connor mcfadden <0717330@schoolsnet.act.edu.au>
2022-03-07 10:30:21 +1030

    making bullet work

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,6 +1,6 @@
 extends KinematicBody2D
 
-var movement_speed = 200
+var movement_speed = 750
 var bulletSource = preload ("res://Bullet/Bullet.tscn")
 
 # Declare member variables here. Examples:
@@ -12,13 +12,18 @@ var bulletSource = preload ("res://Bullet/Bullet.tscn")
 func _ready():
 	set_process(true)
 	set_physics_process(true)
-
+#up command
 func _physics_process(delta):
-	pass
-			
+	if Input. is_action_pressed("ui_up"):
+		if position.y > 10:
+			move_and_collide(Vector2(0, -movement_speed * delta))
+#down command
+	if Input. is_action_pressed("ui_down"):
+		if position.y < 640:
+			move_and_collide(Vector2(0, movement_speed * delta))
+		
 func _process(delta):
-	pass
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	if Input. is_action_just_pressed("fire"):
+			var BulletInstance = bulletSource.instance()
+			bulletInstance.position = Vector2(position.x, position.y-20)
+			get_tree().get_root().add_child(bulletInstance)
